d1d1047314163432489c1d11bd861e06ef0aa5eb
MareBogdan <marebogdan03@gmail.com>
2025-12-05 17:33:36 +0200

    lab7_final

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,106 @@
+﻿using Grpc.Net.Client;
+using GrpcCustomersService;
+using Microsoft.AspNetCore.Mvc;
+
+using GrpcCustomer = GrpcCustomersService.Customer;
+using ModelCustomer = Mare_Bogdan_Lab2_EB.Models.Customer;
+namespace Mare_Bogdan_Lab2_EB.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(GrpcCustomer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new CustomerService.CustomerServiceClient(channel);
+                client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = new CustomerService.CustomerServiceClient(channel);
+            GrpcCustomer customer = client.Get(new CustomerId { Id = id.Value });
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            client.Delete(new CustomerId { Id = id });
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = new CustomerService.CustomerServiceClient(channel);
+            GrpcCustomer customer = client.Get(new CustomerId { Id = id.Value });
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(GrpcCustomer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new CustomerService.CustomerServiceClient(channel);
+
+                client.Update(customer);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(customer);
+        }
+
+    }
+}
